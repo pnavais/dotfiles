f2b5f5aeccac8b8e43fcc5425e63966ce0f2276e
Pablo Navais <pnavais@gmail.com>
2025-03-06 19:10:11 +0100

    Added NvChad rust config

--- dot_config/NvChad/lua/plugins/init.lua
+++ dot_config/NvChad/lua/plugins/init.lua
@@ -13,6 +13,28 @@ return {
     end,
   },
 
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "toml" },
+    tag = "stable",
+    config = function()
+      require("crates").setup {
+        completion = {
+          cmp = {
+            enabled = true,
+          },
+        },
+      }
+      require("cmp").setup.buffer {
+        sources = { { name = "crates" } },
+      }
+    end,
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
