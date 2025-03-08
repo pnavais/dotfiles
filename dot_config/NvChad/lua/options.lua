7c5bd90e4f124d89b60ca2f37d206ca061e51989
Pablo Navais <pnavais@gmail.com>
2025-03-08 01:46:34 +0100

    Added cursor restore

--- dot_config/NvChad/lua/options.lua
+++ dot_config/NvChad/lua/options.lua
@@ -3,4 +3,10 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = 'both' -- to enable cursorline!
+
+-- Restores cursor on exit
+o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
+  .. ",a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
+  .. ",sm:block-blinkwait175-blinkoff150-blinkon175"
+
