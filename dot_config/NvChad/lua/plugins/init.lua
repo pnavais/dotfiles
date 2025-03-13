925061eea2996a0e51f5bf26ef8ebcd460530492
Pablo Navais <pnavais@gmail.com>
2025-03-13 20:11:59 +0100

    Added bash LSP config

--- dot_config/NvChad/lua/plugins/init.lua
+++ dot_config/NvChad/lua/plugins/init.lua
@@ -18,6 +18,7 @@ return {
     version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
   },
+
   {
     "saecki/crates.nvim",
     ft = { "toml" },
@@ -35,13 +36,25 @@ return {
       }
     end,
   },
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+
+  {
+    "andymass/vim-matchup",
+    event = "BufReadPost", -- Load on file opened
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    -- 	opts = {
+    -- 		ensure_installed = {
+    -- 			"vim", "lua", "vimdoc",
+    --      "html", "css"
+    -- 		},
+    -- 	},
+    matchup = {
+      enable = true,
+    },
+  },
 }
