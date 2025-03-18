ea2a44ca1ebd34ebed2117e1c974b47a06ab9fd2
Pablo Navais <pnavais@gmail.com>
2025-03-18 20:58:26 +0100

    Added bash config

--- dot_config/NvChad/lua/options.lua
+++ dot_config/NvChad/lua/options.lua
@@ -1,12 +1,15 @@
 require "nvchad.options"
 
 -- add yours here!
+--
+-- Undercurl
+vim.cmd [[let &t_Cs = "\e[4:3m"]]
+vim.cmd [[let &t_Ce = "\e[4:0m"]]
 
 local o = vim.o
-o.cursorlineopt = 'both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
 
 -- Restores cursor on exit
 o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
   .. ",a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
   .. ",sm:block-blinkwait175-blinkoff150-blinkon175"
-
