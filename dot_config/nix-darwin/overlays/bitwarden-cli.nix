377dabe73f34a40b05d7368e716bff473e676ca7
Pablo Navais <pnavais@gmail.com>
2026-06-02 20:38:01 +0200

    Removed bitwarden overlay

--- dot_config/nix-darwin/overlays/bitwarden-cli.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-  bitwarden-cli = prev.bitwarden-cli.overrideAttrs (oldAttrs: {
-    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ])
-      ++ [ prev.llvmPackages_18.stdenv.cc ];
-    stdenv = prev.llvmPackages_18.stdenv;
-  });
-}
--- dot_config/nix-darwin/overlays/default.nix
+++ dot_config/nix-darwin/overlays/default.nix
@@ -2,8 +2,7 @@
   # Export all overlays as a list
   allOverlays = [
     (import ./obsidian.nix)
-    (import ./bitwarden-cli.nix)
-      (import ./mise.nix)
+    (import ./mise.nix)
     # Add more overlays here
   ];
 }
